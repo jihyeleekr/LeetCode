3a61d5806c9a1617f650a697e48a0daee9bc3c7d
Jihye Lee <102567820+jihyeleekr@users.noreply.github.com>
2026-05-27 01:47:24 -0400

    Time: 448 ms (86.17%), Space: 0B (100%) - LeetHub

---
+++ 0608-tree-node/0608-tree-node.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT t1.id,
+CASE
+    WHEN t1.p_id IS NULL THEN 'Root'
+    WHEN t1.p_id IS NOT NULL AND t2.id IS NULL THEN 'Leaf'
+    ELSE 'Inner'
+END AS type
+FROM Tree t1
+LEFT JOIN Tree t2 ON t1.id = t2.p_id
+GROUP BY t1.id
